ea2a08b63eada16bd21da20c771baa20e9c1b606
shiokiriyama <45187131+shiokiriyama@users.noreply.github.com>
2025-11-05 22:31:45 +0900

    Update rewrite.js

--- bin/rewrite.js
+++ bin/rewrite.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-// 入力
 const parts = [
   // path.join(__dirname, 'src', 'index.html'),
   // path.join(__dirname, 'src', 'header.html'),
@@ -9,26 +8,25 @@ const parts = [
   // path.join(__dirname, 'src', 'footer.html'),
 ];
 
-// 出力
+// input directory
+const templatePath = path.join(__dirname, '../src/index.html');
+let html = fs.readFileSync(templatePath, 'utf8');
+
+// output directory
 const outputDir = path.join(__dirname, '../docs/');
 const outputPath = path.join(outputDir, 'index.html');
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir, { recursive: true });
 }
 
-const templatePath = path.join(__dirname, '../src/index.html');
-let html = fs.readFileSync(templatePath, 'utf8');
-
+// process
 const replacements = {
   '{{PARAGRAPH}}': '静的に書き換えました。',
 };
-
-// プレースホルダーの置換
 for (const [placeholder, value] of Object.entries(replacements)) {
   html = html.replace(new RegExp(placeholder, 'g'), value);
 }
 
-// 上書き保存（あるいは別ファイルに出力しても可）
+// save
 fs.writeFileSync(outputPath, html, 'utf8');
-
 console.log('HTMLテンプレートを静的に書き換えました。');
